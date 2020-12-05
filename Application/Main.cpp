0ce33907ec0ee7c377be78dce63f05a1bcb767ad
Garrick Kilpack <garrickkilpack@hotmail.com>
2020-12-04 21:07:44 -0700

    Shaders now use texture maps, added Transform class. Cleared out old unused code.

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -7,105 +7,14 @@ int main(int argc, char** argv)
 	gk::Engine engine;
 	engine.Startup();
 
-	static float vertices[] =
-	{
-		-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-		 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-		 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-		 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-		-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-		-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
-
-		-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-		-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-
-		-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
-		-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
-		-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
-		-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
-		-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
-		-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
-
-		 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
-		 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
-		 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
-		 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
-		 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
-		 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
-
-		-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
-		 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
-		 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
-		 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
-		-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
-		-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
-
-		-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-		 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-		 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-		 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-		-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-		-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f
-	};
-
-	//static float vertices[] = {
-	//	// front
-	//	-1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
-	//	 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
-	//	 1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
-	//	-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-	//	// back
-	//	-1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-	//	 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-	//	 1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
-	//	-1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
-	//};
-
-	//// Unsigned short - 16bit
-	//static GLushort indices[] =
-	//{
-	//	// front
-	//	0, 1, 2,
-	//	2, 3, 0,
-	//	// right
-	//	1, 5, 6,
-	//	6, 2, 1,
-	//	// back
-	//	7, 6, 5,
-	//	5, 4, 7,
-	//	// left
-	//	4, 0, 3,
-	//	3, 7, 4,
-	//	// bottom
-	//	4, 5, 1,
-	//	1, 0, 4,
-	//	// top
-	//	3, 2, 6,
-	//	6, 7, 3
-	//};
-
 	gk::Program program;
 	program.CreateShaderFromFile("shaders\\phong.vert", GL_VERTEX_SHADER);
 	program.CreateShaderFromFile("shaders\\phong.frag", GL_FRAGMENT_SHADER);
 	program.Link();
 	program.Use();
 
-	//gk::VertexIndexArray vertexArray;
-	//vertexArray.Create("vertex");
-	//vertexArray.CreateBuffer(sizeof(vertices), sizeof(vertices) / (sizeof(float) * 5), vertices);
-	//vertexArray.SetAttribute(0, 3, 5 * sizeof(float), 0);
-	//vertexArray.SetAttribute(1, 2, 5 * sizeof(float), (3 * sizeof(float)));
-	//vertexArray.CreateIndexBuffer(GL_UNSIGNED_SHORT, sizeof(indices) / sizeof(GLushort), indices);
-
 	gk::VertexArray vertexArray;
 	vertexArray.Create("vertex");
-	//vertexArray.CreateBuffer(sizeof(vertices), sizeof(vertices) / (sizeof(float) * 6), vertices);
-	//vertexArray.SetAttribute(0, 3, 6 * sizeof(float), 0);
-	//vertexArray.SetAttribute(1, 3, 6 * sizeof(float), (3 * sizeof(float)));
 
 	std::vector<glm::vec3> positions;
 	std::vector<glm::vec3> normals;
@@ -130,14 +39,12 @@ int main(int argc, char** argv)
 
 	// Uniform
 	glm::mat4 model = glm::mat4(1.0f);
-
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800 / (float)600, 0.01f, 1000.0f);
-
-	glm::vec3 eye{ 0, 0, 3 };
+	glm::vec3 eye{ 0, 0, 5 };
 	glm::mat4 view = glm::lookAt(eye, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 });
 
 	gk::Texture texture;
-	texture.CreateTexture("Textures\\llama.jpg");
+	texture.CreateTexture("Textures\\ogre_diffuse_flip.bmp");
 
 	program.SetUniform("material.ambient", glm::vec3{ 1, 1, 1 });
 	program.SetUniform("material.diffuse", glm::vec3{ 1, 1, 1 });
@@ -145,7 +52,7 @@ int main(int argc, char** argv)
 	program.SetUniform("material.shininess", 32.0f);
 
 	program.SetUniform("light.ambient", glm::vec3{ 0.1f, 0.1f, 0.1f });
-	program.SetUniform("light.diffuse", glm::vec3{ 0, 0, 1 });
+	program.SetUniform("light.diffuse", glm::vec3{ 1, 1, 1 });
 	program.SetUniform("light.specular", glm::vec3{ 1, 1, 1 });
 
 	glm::vec4 light{ 5, 2, 5, 1 };
@@ -192,11 +99,11 @@ int main(int argc, char** argv)
 		}
 		if (engine.GetSystem<gk::InputSystem>()->GetButtonState(SDL_SCANCODE_W) == gk::InputSystem::eButtonState::HELD)
 		{
-			eye.y -= 4 * engine.GetTimer().DeltaTime();
+			eye.z -= 4 * engine.GetTimer().DeltaTime();
 		}
 		if (engine.GetSystem<gk::InputSystem>()->GetButtonState(SDL_SCANCODE_S) == gk::InputSystem::eButtonState::HELD)
 		{
-			eye.y += 4 * engine.GetTimer().DeltaTime();
+			eye.z += 4 * engine.GetTimer().DeltaTime();
 		}
 		view = glm::lookAt(eye, eye + glm::vec3{ 0, 0, -1 }, glm::vec3{ 0, 1, 0 });
 
